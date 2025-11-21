b7ba5ec074d09b134bb908af898dd3d28f492555
Esraa Elsheikh <esraaelsheikh2005@gmail.com>
2025-11-21 17:25:32 +0200

    add patient class

---
+++ src/Patient.java
@@ -0,0 +1,41 @@
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.UUID;
+public class Patient extends User{
+    private String patientStage;
+    private ArrayList<Relative> relatives;
+    private ArrayList<Memory> memories;
+    private ArrayList<Reminder> reminders;
+    public Patient(String name, String Id, String email, String password, String patientStage){
+        super(name, Id, email, password);
+        this.patientStage=patientStage;
+        this.relatives = new ArrayList<>();
+        this.memories = new ArrayList<>();
+        this.reminders = new ArrayList<>();
+    }
+    public String getPatientStage() {
+        return patientStage;
+    }
+    public void setPatientStage(String patientStage) {
+        this.patientStage = patientStage;
+    }
+    public ArrayList<Relative> getRelatives() {
+        return relatives;
+    }
+    public ArrayList<Memory> getMemories() {
+        return memories;
+    }
+    public ArrayList<Reminder> getReminders() {
+        return reminders;
+    }
+
+
+}
+
+
+
+
