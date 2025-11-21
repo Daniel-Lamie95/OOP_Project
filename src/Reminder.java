21eb43ab896f655f9a3209857b1a5d70e3461507
mehrailseddik27 <mehrailsedik@gmail.com>
2025-11-21 16:52:19 +0200

    made reminder

---
+++ src/Reminder.java
@@ -0,0 +1,46 @@
+import java.time.LocalDateTime;
+public class Reminder {
+
+    private String description;
+    private LocalDateTime dateOfReminder;
+    private Boolean isDone = false;
+
+    public Reminder(String Id , String Name, String description, LocalDateTime dateOfReminder) {
+        super(Id , Name);
+        this.description = description;
+        this.dateOfReminder = dateOfReminder;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public LocalDateTime getDate() {
+        return dateOfReminder;
+    }
+    public void setDate(LocalDateTime date) {
+        this.dateOfReminder = date;
+    }
+    public Boolean getDone() {
+        return isDone;
+    }
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+    public void checkStatus() {
+        if (isDone) {
+            System.out.println("This reminder is completed.");
+        } else {
+            System.out.println("This reminder isn't done yet.");
+        }
+    }
+    public void notifyIfDue() {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (!isDone && now.isAfter(dateOfReminder)) {
+            System.out.println("Reminder:" + description + "(for" + getName() +")");
+        }
+    }
+}
