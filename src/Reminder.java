c5406d543a2370dd9557d4e45d67aa61b422140c
mehrailseddik27 <mehrailsedik@gmail.com>
2025-11-21 16:54:59 +0200

    made reminder

--- src/Reminder.java
+++ src/Reminder.java
@@ -1,5 +1,5 @@
 import java.time.LocalDateTime;
-public class Reminder extends PatientInfo{
+public class Reminder extends Patientinfo{
 
     private String description;
     private LocalDateTime dateOfReminder;
