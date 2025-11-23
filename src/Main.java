f52043db8a9ea13cac3de860e40c067450954880
Daniel-Lamie95 <danielseddik95@gmail.com>
2025-11-23 03:37:15 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
